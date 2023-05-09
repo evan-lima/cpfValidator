8ff28b3db95b416ebd000724502d01753d4b2e56
Evandro F Lima <evandrofelipe1333@gmail.com>
2023-05-09 12:19:30 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+const cpfInput = document.querySelector('.cpf-input');
+const buttonValidate = document.querySelector('.btn-validate');
+const result = document.querySelector('.result');
+
+cpfInput.addEventListener('keypress', e => {
+    if(e.keyCode === 13){
+        if(!cpfInput.value) return;
+        cpfValidate(cpfInput.value);
+    }
+});
+buttonValidate.addEventListener('click', e => {
+    if(!cpfInput.value) return;
+    cpfValidate(cpfInput.value);
+});
+
+function cpfValidate(cpfInput){
+    cpf = convertClear(cpfInput);
+    cpfArray = Array.from(cpf);
+
+    cpfArray.pop();
+    cpfArray.pop();
+
+    cpfArray.push(digitCalc(cpfArray));
+    cpfArray.push(digitCalc(cpfArray));
+
+    const validate = convertClear(cpfArray);
+
+    if(cpf === validate) return display(true);
+    display(false);
+}
+
+function display(validation){
+    if(validation){
+    result.textContent = 'Esse CPF é válido!';
+    result.classList.remove('show-result-invalid');
+    result.classList.add('show-result-valid');
+    return;
+    }
+    result.textContent = 'Esse CPF é ínvalido!';
+    result.classList.remove('show-result-valid');
+    result.classList.add('show-result-invalid');
+}
+
+function convertClear(array){
+    array = String(array);
+    return array.replace(/\D+/g, '');
+}
+
+function digitCalc(cpfArray){
+    let indice = ((cpfArray.length) + 1);
+
+    cpfArray = cpfArray.map(num => { 
+        let temp = Number(num) * indice;
+        indice -= 1;
+        return temp;
+    });
+
+    cpfArray = cpfArray.reduce((ac, val) => ac + Number(val), 0);
+
+    return (11 - (Number(cpfArray) % 11));
+
+}
